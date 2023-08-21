0bef8fc46fc3a1c922d2a58443f67c4938c0d2f9
pouyasadri <pouyapaghandeh78@gmail.com>
2023-08-21 18:57:20 +0200

    added private link to admin panel sanity with a custom script

--- src/app/admin/[[...index]]/page.jsx
+++ src/app/admin/[[...index]]/page.jsx
@@ -11,9 +11,51 @@
 
 import {NextStudio} from 'next-sanity/studio'
 import config from '../../../../sanity.config'
-
+import {useEffect} from "react";
+import {createClient} from "next-sanity";
 // Initialize Sanity client
 
 export default function StudioPage() {
+    useEffect(() => {
+        generateAndSetPrivateLinks();
+    }, []);
     return <NextStudio config={config}/>
 }
+
+async function generateAndSetPrivateLinks() {
+    const client = createClient({
+        "projectId": "0a3zpxbs",
+        "dataset": "production",
+        "token": "sk25R4VxZEF65UZySmr1q1BYm1GcaSH0q3V6ykEkGsty8RKcy0WpuWfzo2r9gDcYdgcsi3qzpk8mtHqUgVEBgSgI0ApwJQDXsUeFaOswQVApKObR5chByOaM6dwRUtDQosBjABQgwatiPoMnHZDIowSP5jyIiHR1WVmFpHfw6GDbw9ScW5rC"
+    });
+    // Fetch all articles
+    const articles = await client.fetch(`
+    *[_type == "apartment"]{
+    ...,
+    city->
+    }
+    `);
+
+    // Generate private links and update articles
+    for (const article of articles) {
+        if (article.city && article.name) {
+            const privateLink = generatePrivateLink(article.city.name, article.name); // Generate the private link
+            const updatedArticle = {...article, privateLink};
+
+            console.log(privateLink);
+            await client
+                .patch(article._id)
+                .set({privateLink})
+                .commit();
+        }
+        // // Update the article with the new private link
+
+    }
+}
+
+function generatePrivateLink(city, name) {
+    // Implement your private link generation logic here
+    return `https://hexuvium.com/${city}/${name}`;
+}
+
+
